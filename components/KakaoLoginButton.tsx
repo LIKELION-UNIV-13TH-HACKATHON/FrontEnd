afa7d3a2e37127c533e86bac181830acb1ab37e6
DaeHun Kang <crol0101@naver.com>
2025-08-06 16:07:02 +0900

    [✨Feat] 카카오톡 쇼설 로그인 로직 구현 (#2)

---
+++ components/KakaoLoginButton.tsx
@@ -0,0 +1,21 @@
+import { getProfile, login } from "@react-native-seoul/kakao-login";
+import { Alert, Button } from "react-native";
+
+const KakaoLoginButton = () => {
+  const handleLogin = async () => {
+    try {
+      const result = await login();
+      const profile = await getProfile();
+      console.log("로그인 성공 ✅", result);
+      console.log("프로필 로딩 성공 ✅", profile);
+      Alert.alert("카카오 로그인 성공", JSON.stringify(result));
+    } catch (error) {
+      console.error("로그인 실패 ❌", error);
+      Alert.alert("카카오 로그인 실패", String(error));
+    }
+  };
+
+  return <Button title="카카오 로그인 테스트" onPress={handleLogin} />;
+};
+
+export default KakaoLoginButton;
