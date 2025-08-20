5acb58707cc404df11aa4a1b2287022d181aaf52
DaeHun Kang <crol0101@naver.com>
2025-08-20 16:15:28 +0900

    [✨Feat] 홍보멘트 추천 스크린 구현 (#24)

---
+++ components/shop/setalarmpage/RecommendItem.tsx
@@ -0,0 +1,21 @@
+import { View, Text, Pressable } from "react-native";
+import React from "react";
+import { useAlarmStore } from "@/store/alarm/useAlarmStore";
+
+const RecommendItem = ({
+  item,
+}: {
+  item: { id: number; text: string; type: string };
+}) => {
+  const { updateAlarm } = useAlarmStore();
+  return (
+    <Pressable
+      className="rounded-lg bg-[#F9F9F9] py-6 px-4 my-1.5"
+      onPress={() => updateAlarm({ contents: item.text })}
+    >
+      <Text className="text-[16px] w-[90%]">{item.text}</Text>
+    </Pressable>
+  );
+};
+
+export default RecommendItem;
