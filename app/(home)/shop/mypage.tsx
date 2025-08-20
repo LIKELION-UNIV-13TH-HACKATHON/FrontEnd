39f3854154564c21b064ee6740877c82614ed9ab
DaeHun Kang <crol0101@naver.com>
2025-08-20 17:54:41 +0900

    [✨Feat] mypage ui 추가 (#none)

--- app/(home)/shop/mypage.tsx
+++ app/(home)/shop/mypage.tsx
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import HeaderShop from "@/components/shop/homepage/HeaderShop";
+import BottomTaps from "@/components/shop/homepage/BottomTaps";
+import MypageBody from "@/components/shop/mypage/MypageBody";
 
 const mypage = () => {
   return (
-    <View>
-      <Text>mypage</Text>
+    <View className="flex-1">
+      <HeaderShop />
+      <MypageBody />
+      <BottomTaps />
     </View>
-  )
-}
+  );
+};
 
-export default mypage
+export default mypage;
